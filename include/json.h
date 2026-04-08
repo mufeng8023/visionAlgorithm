97633d7fa77e0861fd2230f8ce68ba63016256ea
gxs <mufeng8023@foxmail.com>
2026-04-08 22:55:16 +0800

    添加json库

---
+++ include/json.h
@@ -0,0 +1,17 @@
+/***
+ * @Author       : gxs
+ * @Date         : 2025-08-09 13:20:17
+ * @LastEditors  : gxs
+ * @LastEditTime : 2025-08-09 13:20:17
+ * @FilePath     : /projectTemplate/include/json.h
+ * @Description  : json头文件
+ * @
+ * @Copyright (c) 2025 by gxs, All Rights Reserved.
+ */
+#ifndef __JSON__H__
+#define __JSON__H__
+
+#include "./nlohmann/json.hpp"
+using json = nlohmann::json;  // 类型重命名
+
+#endif  // !__JSON__H__
